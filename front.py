26e3d67efe46d90f2ae99a705a911119a76c7878
xyzinabox <athirahazwani@gmail.com>
2023-11-11 22:52:57 +0800

    fixed height, modified title

--- front.py
+++ front.py
@@ -8,16 +8,21 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        dcc.Graph(id="table"),
+        dcc.Graph(id="table",style={'height': '600px'}),
         dcc.Interval(id="interval-component", interval=1 * 60 * 1000, n_intervals=0),
     ]
 )
 
+def extract_date(filename):
+    parts = filename.split('_')
+    date_part = parts[1]
+    cycle_part = parts[2]
+    return date_part, cycle_part
 
 @app.callback(Output("table", "figure"), [Input("interval-component", "n_intervals")])
 def update_table(n):
     last_report_fn = os.listdir("./reports")[0]
-
+    cycle_date = extract_date(last_report_fn)
     report_df = pd.read_csv(f"./reports/{last_report_fn}")
     report_df = report_df.fillna("")
     figure = {
@@ -35,7 +40,7 @@ def update_table(n):
                 },
             }
         ],
-        "layout": {"title": f"Report Table - Interval {n}"},
+        "layout": {"title": f"Report Table {cycle_date[0]} - Cycle {cycle_date[1]}"},
     }
     return figure
 
