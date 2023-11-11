d7d1de73d90e02037bb13dd684a79ba12c056679
Anton <anton.loumakos@gmail.com>
2023-11-11 16:37:42 +0200

    add reports with names

--- front.py
+++ front.py
@@ -2,29 +2,24 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import pandas as pd
-import time
+import os
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        dcc.Graph(
-            id="table"),
-        dcc.Interval(
-            id='interval-component',
-            interval=1*60*1000,
-            n_intervals=0
-        ),
+        dcc.Graph(id="table"),
+        dcc.Interval(id="interval-component", interval=1 * 60 * 1000, n_intervals=0),
     ]
 )
 
-@app.callback(
-    Output('table', 'figure'),
-    [Input('interval-component', 'n_intervals')]
-)
+
+@app.callback(Output("table", "figure"), [Input("interval-component", "n_intervals")])
 def update_table(n):
-    global report_df
-    report_df = pd.read_csv('report.csv')
+    last_report_fn = os.listdir("./reports")[0]
+
+    report_df = pd.read_csv(f"./reports/{last_report_fn}")
+    report_df = report_df.fillna("")
     figure = {
         "data": [
             {
@@ -43,6 +38,7 @@ def update_table(n):
         "layout": {"title": f"Report Table - Interval {n}"},
     }
     return figure
-  
+
+
 if __name__ == "__main__":
     app.run_server(debug=True)
