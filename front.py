14e1425d6ce0073fc170e04adef40faa1bdbb416
xyzinabox <athirahazwani@gmail.com>
2023-12-06 12:46:04 +0800

    set col_len as global var n removed unused comp

--- front.py
+++ front.py
@@ -48,6 +48,8 @@ tab_selected_style = {
     
 }
 
+col_len = 0
+
 def extract_date(filename):
     parts = filename.split('_')
     date_part = parts[1]
@@ -73,7 +75,6 @@ app.layout = html.Div(
             dcc.Tab(id='tab3',label='', value='tab-12',style=tab_style, selected_style=tab_selected_style),
             dcc.Tab(id='tab4',label='', value='tab-18',style=tab_style, selected_style=tab_selected_style),
         ]),
-        html.H5(id='title', style={'display':'None'}),
         html.H5(id='current-time', style={'display':'None'}),
         html.Div(id="table-output", style={'textAlign': 'center', 'padding-top': '40px','padding-bottom': '40px','margin': 'auto','display':'inline-block'}),
         dcc.Interval(id="interval-component", interval=1 * 30 * 1000, n_intervals=0),
@@ -83,16 +84,14 @@ app.layout = html.Div(
         html.Div(id='progress-bar',style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'}),
         html.Div([html.Img(src="assets/hello_kitty.gif", style={'width': '6%', 'height': 'auto'}),
     ], style={'bottom': 0, 'left': 0, 'width': '100%'}),
-    html.Div(id='col_len',style={'display':'none'})
 ])
 
 
 @app.callback(
     Output("progress-bar", "children"),
-    [Input('interval-component-countdown', 'n_intervals'),
-     Input('col_len','children')]
+    Input('interval-component-countdown', 'n_intervals')
 )
-def update_progress_bar(n, col_len):
+def update_progress_bar(n):
     if col_len is not None and col_len < 16:
         progress = (n*0.1)%30
         bar = dbc.Progress(value=progress, max=30,style={'margin-bottom':'10px','width': '180px'})
@@ -138,19 +137,17 @@ def cycle_tab(tab_value):
     cycle_hour = tab_value.split('-')[1] if tab_value else '00'
     return cycle_hour
 
-@app.callback(Output("table-output", "children"),
-              Output("title", "children"),
+@app.callback([Output("table-output", "children"),
               Output("refresh_cycle", "children"),
-              Output("col_len", "children"),
               Output('tab1','label'),
               Output('tab2','label'),
               Output('tab3','label'),
-              Output('tab4','label'),
+              Output('tab4','label'),],
               [Input("interval-component", "n_intervals"),
                Input("cycle-selection", "children")])
 def update_table(n, cycle_hour):
 
-    
+    global col_len
 
     refresh = f"Refreshed {n} times"
 
@@ -176,7 +173,7 @@ def update_table(n, cycle_hour):
     try:
         fn = list(filter(r.search, report_ls))[0]
     except:
-        return None, None, None, None, tab1_label, tab2_label, tab3_label, tab4_label
+        return None, None, tab1_label, tab2_label, tab3_label, tab4_label
 
     cycle_date = extract_date(fn)
     filename = f"./reports/{fn}"
@@ -222,10 +219,6 @@ def update_table(n, cycle_hour):
 
     style_data_conditional = style_data_conditional_first_col + style_conditions
 
-    title = cycle_date[0]
-    title = datetime.strptime(title,'%Y-%m-%d')
-    title = title.strftime("%d-%m-%Y")
-    title = f"today's date: {title}"
     
 
     table = dash_table.DataTable(id="table",
@@ -244,7 +237,7 @@ def update_table(n, cycle_hour):
                                  style_data_conditional=style_data_conditional
                                  )
     
-    return html.Div([table]), title, refresh, col_len, tab1_label, tab2_label, tab3_label, tab4_label
+    return html.Div([table]), refresh,tab1_label, tab2_label, tab3_label, tab4_label
 
 
 if __name__ == "__main__":
