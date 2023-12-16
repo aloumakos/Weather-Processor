637e0985fe5a525ff70e1e8ddd4cb1e83f07bb48
Anton <anton.loumakos@gmail.com>
2023-12-16 09:26:00 +0200

    change worker class

--- cron_scripts/start.sh
+++ cron_scripts/start.sh
@@ -2,4 +2,4 @@
 cron
 cd /home/weather
 python bootstrap.py
-gunicorn -w 2 -b 0.0.0.0:8050 -t 60 --log-level DEBUG front:server
+gunicorn --worker-class gevent -b 0.0.0.0:8050 -t 60 --log-level DEBUG front:server
